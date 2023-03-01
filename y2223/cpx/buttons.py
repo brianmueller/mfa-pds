d1f6a7885a629688ba9161750785ca7396aa7cd8
Brian Mueller <brianscottmueller@gmail.com>
2023-02-28 19:00:13 -0500

    Update buttons.py

--- y2223/cpx/buttons.py
+++ y2223/cpx/buttons.py
@@ -18,11 +18,22 @@ while True:
     #     cp.pixels.fill((0, 0, 0))
 
     # CHALLENGE 3
+    # if cp.button_a:
+    #     cp.pixels[2] = (0, 255, 0) # green
+    # else:
+    #     cp.pixels.fill((0, 0, 0))
+    # if cp.button_b:
+    #     cp.pixels[7] = (0, 0, 255) # blue
+    # else:
+    #     cp.pixels.fill((0, 0, 0))
+
+    # BONUS CHALLENGE
+    cp.pixels.fill((0, 0, 0))  # Turn off the NeoPixels if they're on!
     if cp.button_a:
-        cp.pixels[2] = (0, 255, 0) # green
+        cp.pixels[0:5] = [(255, 0, 0)] * 5 # first half
     else:
-        cp.pixels.fill((0, 0, 0))
+        cp.pixels[0:5] = [(0, 0, 0)] * 5
     if cp.button_b:
-        cp.pixels[7] = (0, 0, 255) # blue
+        cp.pixels[5:10] = [(255, 0, 0)] * 5 # second half
     else:
-        cp.pixels.fill((0, 0, 0))
+        cp.pixels[5:10] = [(0, 0, 0)] * 5
