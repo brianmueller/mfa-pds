c2b3936c3464a1f2dbb55274a74424b94899d187
Brian Mueller <brianscottmueller@gmail.com>
2023-03-28 19:04:26 -0400

    Update reaction.py

--- y2223/cpx/reaction.py
+++ y2223/cpx/reaction.py
@@ -6,8 +6,33 @@ cp.pixels.fill((0, 0, 0)) # reset
 
 # time.sleep(1)
 
+
+
+
 while True:
-    if cp.touch_A1:
-        cp.pixels[0] = (0, 255, 0) # green
-    else:
-        cp.pixels[0] = (255, 0, 0) # red
+    players = [
+        {
+          "input": cp.touch_A1,
+          "output": 6 # cp.pixels[6]
+        },
+        {
+          "input": cp.touch_A2,
+          "output": 8
+        }
+    ]
+
+    # print(players[0])
+    # if cp.touch_A1:
+    #     cp.pixels[0] = (0, 255, 0) # green
+    # else:
+    #     cp.pixels[0] = (255, 0, 0) # red
+    
+    for player in players:
+        print(player["input"])
+        if player["input"]:
+            cp.pixels[player["output"]] = (0, 255, 0) # green
+        else:
+            cp.pixels[player["output"]] = (255, 0, 0) # red
+
+
+
